27ce78b996f337d5c6353ecb96d8fb628d139765
davemf1 <112109492+davemf1@users.noreply.github.com>
2023-04-10 23:29:38 -0400

    Add files via upload

---
+++ expressError.js
@@ -0,0 +1,10 @@
+class ExpressError extends Error {
+    constructor(msg, status=500){
+      super();
+      this.msg = msg;
+      this.status = status;
+    }
+  }
+  
+  module.exports = ExpressError; 
+  
